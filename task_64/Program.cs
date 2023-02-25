8f7bcf87b4641047b4e4b3fc670958c486a52491
Evgeniy Nelidin <nelidin@yandex.ru>
2023-02-25 21:30:10 +0300

    add task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//                                 Выполнить с помощью рекурсии.
+
+//                                 N = 5 -> "5, 4, 3, 2, 1"
+//                                 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+string Recursiv(int N)
+{
+    if (N == 1) return N.ToString();
+    else
+    {
+        return N.ToString() +", "+ Recursiv(N - 1);
+    }
+}
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Натуральные числа в промежутке от {N} до 1 = {Recursiv(N)}");
